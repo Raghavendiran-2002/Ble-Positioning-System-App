0e33a1700303103cbd78fc7c7fe24966af3c58f4
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-02-08 23:13:02 +0530

    realtime rssi

--- lib/home-flow/screens/flutter_blue_plus.dart
+++ lib/home-flow/screens/flutter_blue_plus.dart
@@ -1,5 +1,7 @@
-import 'package:ble_positioning_system/home-flow/services/flutter_ble_service.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class Flutter_Blue_Plus extends StatefulWidget {
   const Flutter_Blue_Plus({Key? key}) : super(key: key);
@@ -9,15 +11,44 @@ class Flutter_Blue_Plus extends StatefulWidget {
 }
 
 class _Flutter_Blue_PlusState extends State<Flutter_Blue_Plus> {
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  Timer? timer;
+  List<ScanResult> scanResult = [];
+
   @override
   void initState() {
     super.initState();
-    BluetoothPackage.instance.scanSpecificDevice();
+    // BluetoothPackage.instance.scanSpecificDevice();
+    scanSpecificDevice();
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      print("********************");
+      scanSpecificDevice();
+      // BluetoothPackage.instance.scanSpecificDevice();
+    });
   }
 
   @override
   void dispose() {
+    timer?.cancel();
     super.dispose();
+  }
+
+  void scanSpecificDevice() {
+    flutterBlue.startScan(timeout: Duration(seconds: 1));
+    flutterBlue.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        if (r.device.id.toString() == "94:B9:7E:D5:CD:F6") {
+          if (scanResult.isEmpty) {
+            scanResult.add(r);
+          } else {
+            setState(() {
+              scanResult[0] = r;
+            });
+          }
+        }
+      }
+    });
+    flutterBlue.stopScan();
   }
 
   @override
@@ -25,17 +56,19 @@ class _Flutter_Blue_PlusState extends State<Flutter_Blue_Plus> {
     return SafeArea(
       child: Scaffold(
         body: ListView.builder(
-          itemCount: BluetoothPackage.instance.scanResult.length,
+          itemCount: scanResult.length,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
               leading:
-                  Text("${BluetoothPackage.instance.scanResult[index].rssi}"),
+                  // Text("${BluetoothPackage.instance.scanResult[index].rssi}"),
+                  Text("${scanResult[index].rssi}"),
               trailing: Text(
-                '${BluetoothPackage.instance.scanResult[index].device.id}',
+                '${scanResult[index].device.id}',
+                // '${BluetoothPackage.instance.scanResult[index].device.id}',
                 style: TextStyle(color: Colors.green, fontSize: 15),
               ),
               title: Text(
-                  "${BluetoothPackage.instance.scanResult[index].device.name}"),
+                  "${scanResult[index].device.name}"), // Text("${BluetoothPackage.instance.scanResult[index].device.name}"),
             );
           },
         ),
--- lib/home-flow/services/flutter_ble_service.dart
+++ lib/home-flow/services/flutter_ble_service.dart
@@ -3,22 +3,31 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 class BluetoothPackage {
   FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
   List<ScanResult> scanResult = [];
+
   var SERVICE_UUID = "1c4956ac-ec89-48dd-806e-47275a6f7f94";
   var CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
   var deviceID = "94:B9:7E:D5:CD:F6";
 
   void scanSpecificDevice() {
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
+    flutterBlue.startScan(timeout: Duration(seconds: 1));
     flutterBlue.scanResults.listen((results) {
       for (ScanResult r in results) {
         if (r.device.id.toString() == deviceID) {
-          if (!scanResult.contains(r)) {
+          if (scanResult.isEmpty) {
             scanResult.add(r);
           } else {
-            scanResult.insert(scanResult.indexOf(r), r);
+            scanResult[0] = r;
           }
+          // if (!scanResult.contains(r)) {
+          //   scanResult.add(r);
+          // } else {
+          //   scanResult.insert(scanResult.indexOf(r), r);
+          // }
         }
       }
+      // for (var value in scanResult) {
+      //   print(value);
+      // }
     });
   }
 
