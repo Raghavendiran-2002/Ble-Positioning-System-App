3b008da64c5f9beb7777fdb91889362f16716719
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-02-12 12:11:29 +0530

    Nav bar init

--- lib/home-flow/screens/homescreen.dart
+++ lib/home-flow/screens/homescreen.dart
@@ -10,6 +10,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Schools',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, "/");
+        break;
+      case 1:
+        Navigator.pushNamed(context, "GeoFencing");
+        break;
+      case 2:
+        Navigator.pushNamed(context, "flutterBluePlus");
+        break;
+      case 3:
+        Navigator.pushNamed(context, "flutter_bluetooth_serial");
+        break;
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -24,38 +66,46 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.museum,
+                color: Colors.black,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.bluetooth_audio_sharp,
+                color: Colors.black,
+              ),
+              label: 'GeoFencing',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings_bluetooth_sharp,
+                color: Colors.black,
+              ),
+              label: 'Discovery',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.navigation_outlined,
+                color: Colors.black,
+              ),
+              label: 'Discovery',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ),
         body: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "flutterBluePlus");
-                  },
-                  icon: Icon(
-                    Icons.bluetooth_audio_sharp,
-                    color: Colors.black,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "flutter_bluetooth_serial");
-                  },
-                  icon: Icon(
-                    Icons.settings_bluetooth_sharp,
-                    color: Colors.black,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "GeoFencing");
-                  },
-                  icon: Icon(
-                    Icons.navigation_outlined,
-                    color: Colors.black,
-                  ),
-                ),
                 IconButton(
                   onPressed: () async {
                     FirebaseAuth.instance.signOut();
