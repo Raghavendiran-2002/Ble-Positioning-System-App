7e666e2d6043e516f702eee4dc0e40c405dea10d
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2023-02-13 22:51:20 +0530

    MQTT refractored

--- lib/nwarehouse-flow/screens/nwarehousescreen.dart
+++ lib/nwarehouse-flow/screens/nwarehousescreen.dart
@@ -1,5 +1,6 @@
-import 'package:ble_positioning_system/nwarehouse-flow/services/mqtt_service.dart';
+import 'package:ble_positioning_system/nwarehouse-flow/services/mqtt.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class NwareHouseScreen extends StatefulWidget {
   const NwareHouseScreen({Key? key}) : super(key: key);
@@ -9,25 +10,42 @@ class NwareHouseScreen extends StatefulWidget {
 }
 
 class _NwareHouseScreenState extends State<NwareHouseScreen> {
+  bool status = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    main();
+    mqtt.instance.setup();
+    mqtt.instance.connect();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Text("HI"),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(":hd"),
-            )
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              Text("MQTT"),
+              FlutterSwitch(
+                width: 125.0,
+                height: 55.0,
+                valueFontSize: 25.0,
+                toggleSize: 45.0,
+                value: status,
+                borderRadius: 30.0,
+                padding: 8.0,
+                showOnOff: true,
+                onToggle: (val) {
+                  setState(() {
+                    mqtt.instance.nodePublish(val);
+                    status = val;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/nwarehouse-flow/services/mqtt_const.dart
@@ -0,0 +1,3 @@
+var url = 'ai0z8ux2h97u4-ats.iot.us-east-1.amazonaws.com';
+int port = 8883;
+var clientId = 'esp';
